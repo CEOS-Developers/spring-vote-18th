b929879e0ea54e9df86e59ab799ce65636086743
letskuku <yujung9288@naver.com>
2023-12-14 11:04:46 +0900

    #1 feat: 파트별 파트장 후보 조회 메서드 추가

--- src/main/java/com/sharemindteam/votesystem/partLeader/repository/PartLeaderRepository.java
+++ src/main/java/com/sharemindteam/votesystem/partLeader/repository/PartLeaderRepository.java
@@ -1,9 +1,14 @@
 package com.sharemindteam.votesystem.partLeader.repository;
 
+import com.sharemindteam.votesystem.global.content.Part;
 import com.sharemindteam.votesystem.partLeader.domain.PartLeader;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PartLeaderRepository extends JpaRepository<PartLeader, Long> {
+    List<PartLeader> findAllByPart(Part part, Sort sort);
 }
