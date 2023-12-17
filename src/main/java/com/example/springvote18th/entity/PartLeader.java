ffdcc69ca46c2f9ac8ba68b086e9836bf6b8b412
younggyo <lee4519698@hanmail.net>
2023-12-18 08:46:39 +0900

    feat : create Entity

---
+++ src/main/java/com/example/springvote18th/entity/PartLeader.java
@@ -0,0 +1,47 @@
+package com.example.springvote18th.entity;
+
+import com.example.springvote18th.entity.base.BaseTimeEntity;
+import com.example.springvote18th.entity.enums.Part;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name = "part_leaders")
+public class PartLeader extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToMany(mappedBy = "partLeader")
+    private List<MemberPartLeader> memberPartLeaders = new ArrayList<>();
+
+    @ManyToOne
+    @JoinColumn(name = "project_id", nullable = false)
+    private Project projectId;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private int count;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Part part;
+
+    @Builder
+    public PartLeader(Long id, Project projectId, String name, int count, Part part) {
+        this.id = id;
+        this.projectId = projectId;
+        this.name = name;
+        this.count = count;
+        this.part = part;
+    }
+}
