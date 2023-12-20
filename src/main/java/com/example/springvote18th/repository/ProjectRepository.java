9f984bb82f17c6ff410e376d7d726b5f10f041be
younggyo <lee4519698@hanmail.net>
2023-12-20 23:51:53 +0900

    feat : create projectController, Service, Repository

--- src/main/java/com/example/springvote18th/repository/ProjectRepository.java
+++ src/main/java/com/example/springvote18th/repository/ProjectRepository.java
@@ -3,5 +3,7 @@ package com.example.springvote18th.repository;
 import com.example.springvote18th.entity.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ProjectRepository extends JpaRepository<Project, Long> {
 }
