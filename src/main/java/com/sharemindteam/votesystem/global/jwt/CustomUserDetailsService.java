21ea222a1899458b32c03697b7b0f170723def39
aeyongdodam <kikititi0510@gmail.com>
2023-12-21 17:18:43 +0900

    fix: exception 유형 변경

--- src/main/java/com/sharemindteam/votesystem/global/jwt/CustomUserDetailsService.java
+++ src/main/java/com/sharemindteam/votesystem/global/jwt/CustomUserDetailsService.java
@@ -1,12 +1,11 @@
 package com.sharemindteam.votesystem.global.jwt;
 
 import com.sharemindteam.votesystem.user.domain.User;
-import com.sharemindteam.votesystem.user.exception.LoginIdAlreadyExistsException;
+import com.sharemindteam.votesystem.user.exception.UserNotFoundException;
 import com.sharemindteam.votesystem.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,8 +18,7 @@ public class CustomUserDetailsService implements UserDetailsService {
     @Transactional
     public UserDetails loadUserByUsername(String loginId) {
         User user = userRepository.findByLoginId(loginId)
-                .orElseThrow(LoginIdAlreadyExistsException::new);//todo:userNotFoundException만들어서 고쳐야함
-        System.out.println("user 찾음 : " + user.getPassword());
+                .orElseThrow(() -> new UserNotFoundException(loginId));
         return new CustomUserDetails(user);
     }
 }
