f460b16a841fef9781da635bc05a818c3c6d3622
aeyongdodam <kikititi0510@gmail.com>
2023-12-21 17:27:46 +0900

    fix: 필요없는 파일 삭제

--- src/main/java/com/sharemindteam/votesystem/auth/repository/AuthorityRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sharemindteam.votesystem.auth.repository;
-
-import com.sharemindteam.votesystem.auth.domain.Authority;
-import java.util.Optional;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuthorityRepository extends JpaRepository<Authority, Long> {
-    Optional<Authority> findByAuthorityName(String authorityName);
-}
