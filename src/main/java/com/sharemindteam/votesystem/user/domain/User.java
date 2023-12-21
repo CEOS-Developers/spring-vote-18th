78e714e1644fb524d0d590f2bdd2463cda7b3e76
aeyongdodam <kikititi0510@gmail.com>
2023-12-21 17:04:25 +0900

    feat: user 엔티티에 role 추가

--- src/main/java/com/sharemindteam/votesystem/user/domain/User.java
+++ src/main/java/com/sharemindteam/votesystem/user/domain/User.java
@@ -4,16 +4,21 @@ import com.sharemindteam.votesystem.global.common.BaseEntity;
 import com.sharemindteam.votesystem.global.content.Part;
 import com.sharemindteam.votesystem.global.content.Team;
 import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
@@ -54,15 +59,25 @@ public class User extends BaseEntity {
         this.votedPart = true;
     }
 
+    public void encodePassword(String password) {
+        this.password = password;
+    }
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    private List<Role> roles;
+
     @Builder
     public User(String loginId, String password, String email, String name, Part part, Team team) {
         this.loginId = loginId;
-        this.password = password;//todo: password 암호화
+        this.password = password;
         this.email = email;
         this.name = name;
         this.part = part;
         this.team = team;
         this.votedPart = false;
         this.votedDemoday = false;
+        this.roles = new ArrayList<>() {{
+            add(Role.ROLE_USER);
+        }};
     }
 }
