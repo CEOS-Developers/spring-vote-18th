883defefc9f1e7c99d77b17fc9c279719402de62
aeyongdodam <kikititi0510@gmail.com>
2023-12-21 17:03:18 +0900

    feat: signIn api 생성

--- src/main/java/com/sharemindteam/votesystem/auth/application/AuthService.java
+++ src/main/java/com/sharemindteam/votesystem/auth/application/AuthService.java
@@ -1,7 +1,11 @@
 package com.sharemindteam.votesystem.auth.application;
 
+import com.sharemindteam.votesystem.auth.dto.request.PostLoginRequest;
+import com.sharemindteam.votesystem.global.dto.response.TokenDto;
 import com.sharemindteam.votesystem.user.dto.request.CreateUserRequest;
 
 public interface AuthService {
     void signUp(CreateUserRequest createUserRequest);
+
+    TokenDto signIn(PostLoginRequest postLoginRequest);
 }
