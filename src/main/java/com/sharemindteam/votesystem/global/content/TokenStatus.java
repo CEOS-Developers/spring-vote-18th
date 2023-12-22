9b395a6f8b35e73daafea3e609b9c133ec8c696b
aeyongdodam <kikititi0510@gmail.com>
2023-12-22 15:52:08 +0900

    feat: refreshToken 관련 api 및 함수 생성

---
+++ src/main/java/com/sharemindteam/votesystem/global/content/TokenStatus.java
@@ -0,0 +1,7 @@
+package com.sharemindteam.votesystem.global.content;
+
+public enum TokenStatus {
+    VALID,
+    EXPIRED,
+    INVALID
+}
--- src/main/java/com/sharemindteam/votesystem/global/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/sharemindteam/votesystem/global/jwt/JwtAuthenticationFilter.java
@@ -1,16 +1,20 @@
 package com.sharemindteam.votesystem.global.jwt;
 
+import com.sharemindteam.votesystem.global.content.TokenStatus;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Set;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.AntPathMatcher;
+import org.springframework.util.PathMatcher;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -19,33 +23,49 @@ import org.springframework.web.filter.OncePerRequestFilter;
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private static final Logger logger = LoggerFactory.getLogger(JwtAuthenticationFilter.class);
-    public static final String AUTHORIZATION_HEADER = "Authorization";
+    private final PathMatcher pathMatcher = new AntPathMatcher();
+    private static final Set<String> PERMIT_ALL_PATHS = Set.of(
+            "/auth/signUp",
+            "/auth/signIn",
+            "/auth",
+            "/user/email",
+            "/user/loginId",
+            "/demoday",
+            "/partLeader",
+            "/swagger-ui/**",
+            "/api-docs/**"
+    );
+
     private TokenProvider tokenProvider;
 
     public JwtAuthenticationFilter(TokenProvider tokenProvider) {
         this.tokenProvider = tokenProvider;
     }
 
+    private boolean isPermitAllPath(String requestURI) {
+        return PERMIT_ALL_PATHS.stream().anyMatch(path -> pathMatcher.match(path, requestURI));
+    }
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
-        String jwt = parsingJwt((HttpServletRequest) request);
+        String jwt = tokenProvider.parsingAccessToken((HttpServletRequest) request);
         String requestURI = ((HttpServletRequest) request).getRequestURI();
-        if (StringUtils.hasText(jwt) && tokenProvider.validateAccessToken(jwt)) {
+        TokenStatus tokenStatus = tokenProvider.validateToken(jwt);
+        if (isPermitAllPath(requestURI)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        if (StringUtils.hasText(jwt) && (tokenStatus == TokenStatus.VALID)) {
             Authentication auth = tokenProvider.getAuthentication(jwt);
             SecurityContextHolder.getContext().setAuthentication(auth);
+        } else if (tokenStatus == TokenStatus.EXPIRED) {
+            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+            response.getWriter().write("accessToken expired");
+            return;
         } else {
             logger.debug("Invalid Token, uri: {}", requestURI);
         }
         filterChain.doFilter(request, response);
-    }
-
-    private String parsingJwt(HttpServletRequest request) {
-        String bearerToken = request.getHeader(AUTHORIZATION_HEADER);
-
-        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
-            return bearerToken.substring(7);
-        }
-        return null;
     }
 }
