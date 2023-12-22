5cad8d0e3a728699df9f4cce70c02157d69dcc12
aeyongdodam <kikititi0510@gmail.com>
2023-12-22 23:23:09 +0900

    feat: user controller swagger 작성

--- src/main/java/com/sharemindteam/votesystem/user/presentation/UserController.java
+++ src/main/java/com/sharemindteam/votesystem/user/presentation/UserController.java
@@ -5,6 +5,9 @@ import com.sharemindteam.votesystem.email.exception.InvalidEmailException;
 import com.sharemindteam.votesystem.user.application.UserService;
 import com.sharemindteam.votesystem.user.dto.request.PostEmailRequest;
 import com.sharemindteam.votesystem.user.dto.request.PostLoginIdRequest;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -29,12 +32,24 @@ public class UserController {
         return "hello";
     }
 
+    @Operation(summary = "로그인 아이디 중복 체크", description = "해당 로그인 아이디 중복인지(사용가능한지) 체크합니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "사용 가능한 로그인 아이디"),
+            @ApiResponse(responseCode = "409", description = "이미 존재하는 로그인 아이디")
+    })
     @PostMapping("/loginId")
     public ResponseEntity<Void> isDuplicatedLoginId(@RequestBody PostLoginIdRequest postLoginIdRequest) {
         userService.validateLoginId(postLoginIdRequest.getLoginId());
         return ResponseEntity.ok().build();
     }
 
+    @Operation(summary = "이메일 아이디 중복 체크 후 메일 전송", description = "이메일 중복 체크 && 해당 이메일로 메일 전송합니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "사용 가능한 이메일 아이디 && 메일 전송 성공"),
+            @ApiResponse(responseCode = "409", description = "이미 존재하는 이메일"),
+            @ApiResponse(responseCode = "400", description = "이메일 형식이 올바르지 않음"),
+            @ApiResponse(responseCode = "403", description = "유효기간(3분)이 지나지 않은 이메일 코드가 남아있음")
+    })
     @PostMapping("/email")
     public ResponseEntity<Void> isDuplicatedEmail(@Valid @RequestBody PostEmailRequest postEmailRequest,
                                                   Errors errors) {
